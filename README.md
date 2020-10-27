2d6c43f87cb6ff69238fe1a58b3c4df1214dfb75
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2020-10-28 01:17:29 +0900

    added readme and basic interaction (#1)

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# slack-in-your-time
- A Slack application that can convert your time to everyone else's
+# Slack In Your Time
+
+This Slack application will automatically change any messages that references time into everyone's timezone.
+
+## How to use
+
+Before deploying this app to a server, please set up the environmental variables in the .env file.
+
+```env
+SLACK_SIGNING_SECRET=
+SLACK_BOT_TOKEN=
+PORT=3000
+```
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,75 @@
 import { App } from '@slack/bolt';
 
-// Initializes your app with your bot token and signing secret
-const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-});
+export default function main() {
+    // Initializes your app with your bot token and signing secret
+    const app = new App({
+        token: process.env.SLACK_BOT_TOKEN,
+        signingSecret: process.env.SLACK_SIGNING_SECRET,
+    });
 
-(async () => {
-    // Start your app
-    await app.start(process.env.PORT || 3000);
+    // Listens to incoming messages that contain "hello"
+    app.message('change time', async ({ message, say }) => {
+        // say() sends a message to the channel where the event was triggered
+        console.log('someone said ' + message.text);
+        // say() sends a message to the channel where the event was triggered
+        await say({
+            blocks: [
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text:
+                            "Hey! Looks like you said: *{local_date}*\nWould you like me to convert this to everyone's local time?",
+                    },
+                },
+                {
+                    type: 'actions',
+                    elements: [
+                        {
+                            type: 'button',
+                            text: {
+                                type: 'plain_text',
+                                emoji: true,
+                                text: 'Yes',
+                            },
+                            style: 'primary',
+                            value: 'click_me_123',
+                            action_id: 'btn_okay',
+                        },
+                        {
+                            type: 'button',
+                            text: {
+                                type: 'plain_text',
+                                emoji: true,
+                                text: 'No',
+                            },
+                            style: 'danger',
+                            value: 'click_me_123',
+                            action_id: 'btn_decline',
+                        },
+                    ],
+                },
+            ],
+            text: `Hey there <@${message.user}>!`,
+        });
+    });
 
-    console.log('⚡️ Bolt app is running!');
-})();
+    app.action('btn_okay', async ({ body, ack, say }) => {
+        // Acknowledge the action
+        await ack();
+        await say(`<@${body.user.id}> clicked the button`);
+    });
+
+    app.action('btn_decline', async ({ body, ack, say }) => {
+        // Acknowledge the action
+        await ack();
+        await say(`no? Really?!`);
+    });
+
+    (async () => {
+        // Start your app
+        await app.start(process.env.PORT || 3000);
+
+        console.log('⚡️ Bolt app is running!');
+    })();
+}
